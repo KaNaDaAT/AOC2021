a707cdbd0a82a8b13fbaef9770f73c68e1fe3c09
kdanzer-tgm <44087538+kdanzer-tgm@users.noreply.github.com>
2021-12-18 23:59:44 +0100

    Day 17

--- AdventOfCoding/Aufgaben/Aufgabe17A.cs
+++ AdventOfCoding/Aufgaben/Aufgabe17A.cs
@@ -1,23 +1,28 @@
 ﻿using Lib;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCoding.Aufgaben {
 	public class Aufgabe17A : AufgabeAbstract {
 
 		protected override void Runner(Reader reader) {
-			string[] lines = reader.ReadAndGetLines();
+			string input = reader.ReadAll();
+			input = input.Replace("target area: ", "").Replace("x=", "").Replace("y=", "");
+			var inputs = input.Split(new string[] { ", ", ".." }, StringSplitOptions.RemoveEmptyEntries);
+			int[] Bounds = new int[4]; // (xx xy) (yx yy)
+			Bounds[0] = int.Parse(inputs[0]);
+			Bounds[1] = int.Parse(inputs[1]);
+			Bounds[2] = int.Parse(inputs[2]);
+			Bounds[3] = int.Parse(inputs[3]);
 
-
-			for(int i = 0; i < lines.Length; i++) {
-
-			}
-
-
-			/* Output Region */
-			if(!IsDebug())
-				return;
+			this.result = GetMaxY(ref Bounds);
 		}
 
+		private int GetMaxY(ref int[] Bounds) {
+			return Bounds[2] * (Bounds[2] + 1) / 2;
+		}
 	}
 }
--- AdventOfCoding/Aufgaben/Aufgabe17B.cs
+++ AdventOfCoding/Aufgaben/Aufgabe17B.cs
@@ -6,17 +6,39 @@ namespace AdventOfCoding.Aufgaben {
 	public class Aufgabe17B : AufgabeAbstract {
 
 		protected override void Runner(Reader reader) {
-			string[] lines = reader.ReadAndGetLines();
+			string input = reader.ReadAll();
+			input = input.Replace("target area: ", "").Replace("x=", "").Replace("y=", "");
+			var inputs = input.Split(new string[] { ", ", ".." }, StringSplitOptions.RemoveEmptyEntries);
+			int[] Bounds = new int[4]; // (xx xy) (yx yy)
+			Bounds[0] = int.Parse(inputs[0]);
+			Bounds[1] = int.Parse(inputs[1]);
+			Bounds[2] = int.Parse(inputs[2]);
+			Bounds[3] = int.Parse(inputs[3]);
 
+			this.result = GetCount(ref Bounds);
+		}
 
-			for(int i = 0; i < lines.Length; i++) {
-
+		private int GetCount(ref int[] Bounds) {
+			int maxY = Bounds[2] * (Bounds[2] + 1) / 2;
+			int count = 0;
+			for(int x = 0; x < Bounds[1] * 2; x++) {
+				for(int y = Bounds[2]; y < Math.Sqrt(maxY - Bounds[2]); y++) {
+					int velocityX = x;
+					int velocityY = y;
+					int curX = velocityX, curY = velocityY;
+					while(curX <= Bounds[1] && curY >= Bounds[2]) {
+						if(curX >= Bounds[0] && curX <= Bounds[1] && curY >= Bounds[2] && curY <= Bounds[3]) {
+							count++;
+							break;
+						}
+						velocityX = velocityX <= 0 ? 0 : velocityX - 1;
+						velocityY--;
+						curX += velocityX;
+						curY += velocityY;
+					}
+				}
 			}
-
-
-			/* Output Region */
-			if(!IsDebug())
-				return;
+			return count;
 		}
 
 	}
